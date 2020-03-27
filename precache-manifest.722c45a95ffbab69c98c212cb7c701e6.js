f8348bda64fa4a336e7ccab6038dd5b34beffaa2
bagasnur <bagasnur00@mail.ugm.ac.id>
2020-03-27 22:53:39 +0700

    Updates

---
+++ precache-manifest.722c45a95ffbab69c98c212cb7c701e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0990812a4bd14702771807f860d733",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/index.html"
+  },
+  {
+    "revision": "92ba68409f332904bec4",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "79df3b4c194992edb24a",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/static/css/main.cd1490e1.chunk.css"
+  },
+  {
+    "revision": "92ba68409f332904bec4",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/static/js/2.716d4d1b.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/static/js/2.716d4d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79df3b4c194992edb24a",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/static/js/main.c69b35ab.chunk.js"
+  },
+  {
+    "revision": "bb02e1ea0cd128b09b0a",
+    "url": "/WEB3B-431730-BAGAS_NUR-REACT-DEPLOY/static/js/runtime-main.a4e19295.js"
+  }
+]);
